92c85ac12b12244cbb75f891d49e1e87cda214fe
mrunal2022 <mrunalghodke2022@gmail.com>
2021-11-27 23:13:24 +0530

    http

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -13,8 +14,11 @@ const appRoutes: Routes = [
       { path: '', component: RecipeStartComponent },
 
       { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailComponent },
-      { path: ':id/edit', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
+      {
+        path: ':id/edit', component: RecipeEditComponent,
+        resolve: [RecipesResolverService]
+      },
 
     ]
   },
@@ -25,5 +29,6 @@ const appRoutes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,19 +6,25 @@ import { Recipe } from "./recipe.model";
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
-    private recipes: Recipe[] = [
-        new Recipe('Chicken Biryani', 'Chicken Biryani is a delicious savory rice dish that is loaded with spicy marinated chicken, caramelized onions, and flavorful saffron rice', 'https://geekrobocook.com/wp-content/uploads/2021/05/Muradabadi-chicken-biryani.jpg', [
-            new Ingredient('Chicken', 1),
-            new Ingredient('Onions', 4),
-            new Ingredient('Tomatoes', 3)
-        ]),
-        new Recipe('Dosa', 'Dosa is a popular South Indian thin crepe made with fermented rice and lentil batter.', 'https://wallpapercave.com/wp/wp6734909.jpg', [
-            new Ingredient('Rice', 2),
-            new Ingredient('Dal', 5)
-
-        ])
-    ];
+    /*private recipes: Recipe[] = [
+         new Recipe('Chicken Biryani', 'Chicken Biryani is a delicious savory rice dish that is loaded with spicy marinated chicken, caramelized onions, and flavorful saffron rice', 'https://geekrobocook.com/wp-content/uploads/2021/05/Muradabadi-chicken-biryani.jpg', [
+             new Ingredient('Chicken', 1),
+             new Ingredient('Onions', 4),
+             new Ingredient('Tomatoes', 3)
+         ]),
+         new Recipe('Dosa', 'Dosa is a popular South Indian thin crepe made with fermented rice and lentil batter.', 'https://wallpapercave.com/wp/wp6734909.jpg', [
+             new Ingredient('Rice', 2),
+             new Ingredient('Dal', 5)
+ 
+         ])
+     ];*/
+    private recipes: Recipe[] = [];
     constructor(private slService: ShoppingListService) {
+
+    }
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
 
     }
     getRecipes() {
